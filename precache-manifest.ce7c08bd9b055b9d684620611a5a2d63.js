0a283e8c1b7cfcc0027466d4a3d45a244946b2cd
Sravan Kumar <sravangorati2001@gmail.com>
2020-09-27 18:13:47 +0530

    Updates

---
+++ precache-manifest.ce7c08bd9b055b9d684620611a5a2d63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0f76ad0037920aed336ff80ecef3b1",
+    "url": "/TO-DO-App/index.html"
+  },
+  {
+    "revision": "21644243ddb4c4cddc01",
+    "url": "/TO-DO-App/static/css/main.10bc2844.chunk.css"
+  },
+  {
+    "revision": "a508aac9b7a57e50f5f2",
+    "url": "/TO-DO-App/static/js/2.205ad782.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TO-DO-App/static/js/2.205ad782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21644243ddb4c4cddc01",
+    "url": "/TO-DO-App/static/js/main.a3f93c48.chunk.js"
+  },
+  {
+    "revision": "ca62a0a18348f723bd15",
+    "url": "/TO-DO-App/static/js/runtime-main.2816d871.js"
+  },
+  {
+    "revision": "e7d90f718afbdf50ff5f51cc770eed91",
+    "url": "/TO-DO-App/static/media/image2.e7d90f71.jpg"
+  }
+]);
